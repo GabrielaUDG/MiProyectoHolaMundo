e2c6dd320954f6f3e9d4f5dd90223ecfb7f06b67
GabrielaUDG <tuemail@example.comluz.alvarado@udgvirtual.udg.mx>
2024-09-05 18:32:09 -0600

    Primer commit: Configuración inicial del proyecto de Unity

---
+++ Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola mundo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
